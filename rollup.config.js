c7958e72a54ea53fa7dba5a35785f40e9c53f280
mannie.exe <manpreet.mannie.singh@gmail.com>
2022-04-21 08:23:23 -0700

    Revert Rollup config file name to `.js`
    - only default exports work otherwise

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
-/* eslint-disable import/no-extraneous-dependencies */
+import pkg from './package.json';
+import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
@@ -7,7 +8,25 @@ import { terser } from 'rollup-plugin-terser';
 const isDev = process.env.NODE_ENV === 'dev' ? true : false;
 const isProd = process.env.NODE_ENV === 'prod' ? true : false;
 
+const config = {
+  input: './src/extension.js',
+  output: {
+    file: pkg.main,
+    format: 'cjs',
+    sourcemap: isDev,
+  },
+  external: [
+    'assert',
+    'fs',
+    'module',
+    'path',
+    'util',
+    'vscode',
+  ],
+};
+
 const plugins = [
+  json(),
   resolve({ preferBuiltins: true }),
   commonjs(),
   replace({
@@ -22,18 +41,6 @@ if (isProd)
   plugins.push(terser());
 
 export default {
-  input: './src/extension.js',
-  output: {
-    file: './dist/extension.js',
-    format: 'cjs',
-    sourcemap: isDev,
-  },
-  external: [
-    'vscode',
-    'fs',
-    'path',
-    'module',
-    'util',
-  ],
+  ...config,
   plugins,
 };
